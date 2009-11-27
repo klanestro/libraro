50d6b465c776200423dd7e323c01342e26c2827b
Alexei Boronine <alexei.boronine@gmail.com>
2009-11-27 14:25:53 -0500

    Add support for cleaning XML files

--- scripts.py
+++ scripts.py
@@ -3,6 +3,7 @@
 import tarfile
 import sys
 import os
+import re
 
 # scripts.py file location
 scripts = os.path.normpath(os.path.join(os.getcwd(), __file__))
@@ -11,6 +12,8 @@ libraro = os.path.dirname(scripts)
 # Allow to import models
 sys.path.append(os.path.split(libraro)[0])
 os.environ["DJANGO_SETTINGS_MODULE"] = "libraro.settings"
+
+from libraro.settings import MEDIA_ROOT
 
 def pack():
 	tar = tarfile.open(libraro + "/packed.tar", "w")
@@ -30,20 +33,64 @@ helpstring = """
 The following commands are implemented:
 
 ./scripts.py pack
-   Puts libraro.sqlite and the contents of media/works
-   into an archive packed.tar. This archive can be pushed
-   to github, unlike the database and the text files.
-   packed.tar is overwritten.
+	Puts libraro.sqlite and the contents of media/works
+	into an archive packed.tar. This archive can be pushed
+	to github, unlike the database and the text files.
+	packed.tar is overwritten.
 
 ./scripts.py unpack
-   Unpacks packed.tar, overwriting everything.
+	Unpacks packed.tar, overwriting everything.
 
 ./scripts.py rebuild [id]
-   For the work with the given id, it deletes the cached
-   pages and all converted files, leaving only the raw XML 
-   files, then rebuilds everything. If id is omitted, it
-   does this for every work.
+	For the work with the given id, it deletes the cached
+	pages and all converted files, leaving only the raw XML 
+	files, then rebuilds everything. If id is omitted, it
+	does this for every work.
+
+./scripts.py clean
+	Performs standard cleaning operations on each raw XML file
+	such as removing all unnecessary XML and HTML entites.
 """
+
+def unescape(text): # http://stackoverflow.com/users/62262/karlcow
+	"""Removes HTML or XML character references 
+	and entities from a text string.
+	@param text The HTML (or XML) source text.
+	@return The plain text, as a Unicode string, if necessary.
+	from Fredrik Lundh
+	2008-01-03: input only unicode characters string.
+	http://effbot.org/zone/re-sub.htm#unescape-html
+	"""
+	def fixup(m):
+		text = m.group(0)
+		if text[:2] == "&#":
+		# character reference
+			try:
+				if text[:3] == "&#x":
+					return unichr(int(text[3:-1], 16))
+				else:
+					return unichr(int(text[2:-1]))
+			except ValueError:
+				print "Value Error"
+				pass
+		else:
+		# named entity
+		# reescape the reserved characters.
+			try:
+				if text[1:-1] == "amp":
+					text = "&amp;amp;"
+				elif text[1:-1] == "gt":
+					text = "&amp;gt;"
+				elif text[1:-1] == "lt":
+					text = "&amp;lt;"
+				else:
+					print text[1:-1]
+					text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+			except KeyError:
+				print "keyerror"
+				pass
+			return text # leave as is
+	return re.sub("&#?\w+;", fixup, text)
 
 if __name__ == "__main__":
 
@@ -67,3 +114,10 @@ if __name__ == "__main__":
 				rebuild(work)
 	elif sys.argv[1] == "help":
 		print helpstring
+	elif sys.argv[1] == "clean":
+		from pages.models import Work
+		for work in Work.objects.all():
+			filename = "%sworks/%d/work.xml" % (MEDIA_ROOT, work.id)
+			clean = unescape(open(filename, "r").read().decode("utf-8"))
+			open(filename, "w").write(clean.encode("utf-8"))
+			print "Cleaned %s" % filename
