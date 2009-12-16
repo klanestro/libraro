f5ee7a1b508160ad4d32de7c4420bdff144fcbed
Alexei Boronine <alexei.boronine@gmail.com>
2009-12-16 00:21:18 -0500

    replace ROOT_FOLDER with PROJECT_ROOT

--- urls.py
+++ urls.py
@@ -1,18 +1,10 @@
 from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from settings import ROOT_FOLDER
+from settings import PROJECT_ROOT
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^esp/', include('esp.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
 	(r'^$', 'libraro.pages.views.home'),
 	(r'^robots.txt$', 'libraro.pages.views.robots'),
 	(r'^read/$', 'libraro.pages.views.list_recent'),
@@ -22,6 +14,6 @@ urlpatterns = patterns('',
 	(r'^read/(?P<author>\w*)/(?P<title>\w*)/print.html$', 'libraro.pages.views.htmlpage'),
 	(r'^read/(?P<author>\w*)/(?P<title>\w*)/$', 'libraro.pages.views.page'),
 	(r'^read/(?P<author>\w*)/(?P<title>\w*)/(?P<page>\w*)/$', 'libraro.pages.views.page'),
-	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': ROOT_FOLDER+'/media/'}),
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PROJECT_ROOT+'/media/'}),
     (r'^admin/(.*)', admin.site.root),
 )
